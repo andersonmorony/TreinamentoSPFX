9154615b464e4744600ca6dfdeb44046e06406ff
andersonmorony <andersonmoroni92@gmail.com>
2021-09-23 09:36:56 -0300

    Codigos do encontro 1 e 2

---
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'HelloWorldWebPartStrings';
+import HelloWorld from './components/HelloWorld';
+import { IHelloWorldProps } from './components/IHelloWorldProps';
+import { PropertyFieldNumber } from '@pnp/spfx-property-controls/lib/PropertyFieldNumber';
+import { PropertyFieldListPicker, PropertyFieldListPickerOrderBy } from '@pnp/spfx-property-controls/lib/PropertyFieldListPicker';
+
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+
+export interface IHelloWorldWebPartProps {
+  description: string;
+  Title: string;
+  phone: number;
+  numberValue: number;
+  lists: string | string[];
+}
+
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+
+
+
+// ...
+
+public onInit(): Promise<void> {
+
+  return super.onInit().then(_ => {
+
+    sp.setup({
+      spfxContext: this.context
+    });
+  });
+}
+
+
+  public render(): void {
+    const element: React.ReactElement<IHelloWorldProps> = React.createElement(
+      HelloWorld,
+      {
+        description: this.properties.description,
+        Title: this.properties.Title,
+        phone: this.properties.phone,
+        numberValue: this.properties.numberValue,
+        lists: this.properties.lists
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: "Aula 1"
+          },
+          groups: [
+            {
+              groupName: "Informações",
+              groupFields: [
+                PropertyPaneTextField('Title', {
+                  label: "Titulo"
+                }),
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneTextField('phone', {
+                  label: "Telefone"
+                }),
+                PropertyFieldNumber("numberValue", {
+                  key: "numberValue",
+                  label: "Number value only",
+                  description: "Number field description",
+                  value: this.properties.numberValue,
+                  maxValue: 10,
+                  minValue: 1,
+                  disabled: false
+                }),
+                PropertyFieldListPicker('lists', {
+                  label: 'Select a list',
+                  selectedList: this.properties.lists,
+                  includeHidden: false,
+                  orderBy: PropertyFieldListPickerOrderBy.Title,
+                  disabled: false,
+                  onPropertyChange: this.onPropertyPaneFieldChanged.bind(this),
+                  properties: this.properties,
+                  context: this.context,
+                  onGetErrorMessage: null,
+                  deferredValidationTime: 0,
+                  key: 'listPickerFieldId'
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/helloWorld/components/IHelloWorldProps.ts
@@ -0,0 +1,7 @@
+export interface IHelloWorldProps {
+  description: string;
+  Title: string;
+  phone: number;
+  numberValue: number;
+  lists: string | string[];
+}
